829a7b2002973f7ca99959d35222786601850ec3
safiy-safy <137833469+safiy-safy@users.noreply.github.com>
2023-12-02 00:09:05 -0800

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        my_tuple = (0, None)
-    else:
-        my_tuple = (len(sentence), sentence[:1])
-    return(my_tuple)
+    """
+    Args:
+        sentence: a string argument
+
+    Returns:
+        a tuple with the length of a string and its first character
+    """
+
+    str_len, first_char = len(sentence), sentence[0]
+    return (str_len, first_char)
