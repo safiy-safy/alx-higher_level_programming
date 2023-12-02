0b476e61b1c0220391996cef032c68851174ceaf
safiy-safy <137833469+safiy-safy@users.noreply.github.com>
2023-12-02 00:13:21 -0800

    7. Tuples addition

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -10,3 +10,9 @@ def multiple_returns(sentence):
 
     str_len, first_char = len(sentence), sentence[0]
     return (str_len, first_char)
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        my_tuple = (0, None)
+    else:
+        my_tuple = (len(sentence), sentence[:1])
+    return(my_tuple)
