11ddc7516acf4193a2e1c1d57fbf7a43086efde3
safiy-safy <137833469+safiy-safy@users.noreply.github.com>
2023-12-02 02:01:25 -0800

    6. Build my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-from sys import argv
+from sys import argv, exit
 from calculator_1 import add, sub, mul, div
-if __name__ != "__main__":
-    exit()
 
-argc = len(argv) - 1
-if argc != 3:
-    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
-    exit(1)
-elif argv[2] == '+':
-    func = add
-elif argv[2] == '-':
-    func = sub
-elif argv[2] == '*':
-    func = mul
-elif argv[2] == '/':
-    func = div
-else:
-    print("Unknown operator. Available operators: +, -, *, and /")
-    exit(1)
+if __name__ == '__main__':
+    argv.pop(0)
+    argvlength = len(argv)
 
-result = func(int(argv[1]), int(argv[3]))
-print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
-    argv[2], int(argv[3]), result))
+    if (argvlength == 3):
+        a = int(argv[0])
+        b = int(argv[2])
+
+        if (argv[1] == "+"):
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif (argv[1] == "-"):
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif (argv[1] == "*"):
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        elif (argv[1] == "/"):
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
