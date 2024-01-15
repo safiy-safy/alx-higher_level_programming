5d314a41af676aa7309d889c3954ed9f08e9d838
safiy-safy <137833469+safiy-safy@users.noreply.github.com>
2024-01-15 07:50:15 -0800

    2. First Rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Module for Rectangle class.'''
+from models.base import Base
+
+
+class Rectangle(Base):
+    '''A Rectangle class.'''
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        '''Constructor.'''
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
