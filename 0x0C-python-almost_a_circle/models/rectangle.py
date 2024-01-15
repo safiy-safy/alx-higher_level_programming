ecab944c4aa0c934ae8e0f0fc1827dd6fdb1ee5d
safiy-safy <137833469+safiy-safy@users.noreply.github.com>
2024-01-15 02:40:53 -0800

    4. Area first

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -61,4 +61,7 @@ class Rectangle(Base):
             raise ValueError("{} must be >= 0".format(name))
         elif not eq and value <= 0:
             raise ValueError("{} must be > 0".format(name))
+ef area(self):
+        '''Computes area of this rectangle.'''
+        return self.width * self.height
 
